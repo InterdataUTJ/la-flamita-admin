b7ebcd718610431fe094ead120aa3cfe8a186ed4
IsmaCortGtz <ismacortgtz@gmail.com>
2024-10-26 18:01:27 -0600

    Login is now working

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\Auth\AuthEmpleadoController;
 use App\Http\Controllers\Auth\AuthClienteController;
+use App\Http\Controllers\Auth\GoogleController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -21,8 +22,12 @@ Route::view('/', "menu")->name('menu');
 // Clientes
 Route::get('/login', [AuthClienteController::class, 'showForm'])->name('cliente.login');
 Route::post('/login', [AuthClienteController::class, 'login'])->name('cliente.login');
+
 Route::get('/singup', [AuthClienteController::class, 'showNewForm'])->name('cliente.singup');
 Route::post('/singup', [AuthClienteController::class, 'create'])->name('cliente.singup');
+
+Route::get("/auth/google/redirect", [GoogleController::class, "redirect"]);
+Route::get("/auth/google/callback", [GoogleController::class, "callback"]);
 
 // Empleados
 Route::get('/empleado/login', [AuthEmpleadoController::class, 'showForm'])->name('empleado.login');
