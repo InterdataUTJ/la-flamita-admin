0efb1c6ba5a11912402fb10824b7a83fbd913e90
IsmaCortGtz <ismacortgtz@gmail.com>
2025-03-11 17:42:52 -0600

    React API services created

---
+++ react/src/services/Categorias/types.d.ts
@@ -0,0 +1,17 @@
+export interface CategoriaDato {
+  _id: string;
+  nombre: string;
+}
+
+export interface CategoriaResponse {
+  _id: string;
+  datos: number | CategoriaDato[];
+  nombre: string;
+  descripcion: string;
+}
+
+export interface CategoriaRequest {
+  nombre?: string;
+  descripcion?: string;
+  valores?: string[];
+}
---
+++ react/src/services/Empleados/index.ts
@@ -0,0 +1,24 @@
+import Http from "../base";
+import { EmpleadoResponse, EmpleadoRequest } from "./types";
+
+export default class EmpleadoService {
+  static listar(jwt: string): Promise<EmpleadoResponse[]> {
+    return Http.get<EmpleadoResponse[]>("/empleado/listar", { jwt }) as Promise<EmpleadoResponse[]>;
+  }
+
+  static crear(jwt: string, empleado: EmpleadoRequest) {
+    return Http.post<undefined>("/empleado/crear", empleado, { jwt, asForm: true });
+  }
+
+  static editar(jwt: string, id: string, empleado: EmpleadoRequest) {
+    return Http.put<undefined>(`/empleado/editar/${id}`, empleado, { jwt, asForm: true });
+  }
+
+  static mostrar(jwt: string, id: string): Promise<EmpleadoResponse[]> {
+    return Http.get<EmpleadoResponse[]>(`/empleado/mostrar/${id}`, { jwt }) as Promise<EmpleadoResponse[]>;
+  }
+
+  static eliminar(jwt: string, id: string) {
+    return Http.delete<undefined>(`/empleado/eliminar/${id}`, { jwt });
+  }
+}
