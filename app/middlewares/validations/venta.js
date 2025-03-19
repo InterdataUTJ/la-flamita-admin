04f9e26c02b5d947f8e7daa5a3717ba49f16de5b
IsmaCortGtz <ismacortgtz@gmail.com>
2025-03-19 15:00:35 -0600

    Fix producto cantidad when a new venta is created

--- app/middlewares/validations/venta.js
+++ app/middlewares/validations/venta.js
@@ -6,6 +6,7 @@ export default function validate(method) {
   switch (method) {
     case "crear": {
       return [
+        body("metodo_pago", "Debes proporcionar un metodo de pago valido").exists().isString().trim().notEmpty().isIn(["EFECTIVO", "TARJETA", "PAYPAL"]),
         body("productos", "No hay productos en la venta").exists().isObject().notEmpty(),
         number("productos.*", { articulo: "el" }),
         checkValidationResult,
@@ -15,7 +16,7 @@ export default function validate(method) {
     case "editar": {
       return [
         param("ventaId", "Falta el id de la venta").exists().isString().trim().notEmpty(),
-        text("metodo_pago", { optional: true }),
+        body("metodo_pago", "Debes proporcionar un metodo de pago valido").optional().isString().trim().notEmpty().isIn(["EFECTIVO", "TARJETA", "PAYPAL"]),
         checkValidationResult,
       ];
     }
