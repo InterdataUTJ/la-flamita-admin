2c980d28095044e0db5c31e778cb4fc9539993ac
IsmaCortGtz <ismacortgtz@gmail.com>
2025-03-26 12:59:23 -0600

    Make mongoose use ARI from env

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -2,9 +2,10 @@ import mongoose from 'mongoose';
 import print from '#util/print/index.js';
 
 try {
-  const { MONGODB_USER, MONGODB_PASSWORD, MONGODB_HOST, MONGODB_DATABASE } = process.env;
+  const { MONGODB_URI, MONGODB_DATABASE, MONGODB_REPLICASET } = process.env;
   print.log(`Connecting to MongoDB...`);
-  await mongoose.connect(`mongodb+srv://${MONGODB_USER}:${MONGODB_PASSWORD}@${MONGODB_HOST}/${MONGODB_DATABASE}`); 
+  const replica = Boolean(MONGODB_REPLICASET) ? `?replicaSet=${MONGODB_REPLICASET}` : '';
+  await mongoose.connect(`${MONGODB_URI}/${MONGODB_DATABASE}${replica}`); 
 } catch(err) {
   print.error(err.message);
 }
